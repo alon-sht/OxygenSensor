bd48b0b9f1cda35eecc87d9c1f346d6e8169c492
Alon <alon.shtrikman@gmail.com>
2023-06-13 15:21:04 +0300

    Update OxySense.ino

--- OxySense.ino
+++ OxySense.ino
@@ -1,30 +1,19 @@
+
 #include <SoftwareSerial.h>
-#include <Wire.h> // Library for I2C communication
-#include <LiquidCrystal_I2C.h> // Library for LCD
-#include <TM1637Display.h>
-
-// // Define the connections pins
-// #define CLK 3
-// #define DIO 4
-// // Create a display object of type TM1637Display
-// TM1637Display display = TM1637Display(CLK, DIO);
-// // Create an array that turns all segments ON
-// const uint8_t allON[] = {0xff, 0xff, 0xff, 0xff};
-
-// // Create an array that turns all segments OFF
-// const uint8_t allOFF[] = {0x00, 0x00, 0x00, 0x00};
+#include <Wire.h>               // Library for I2C communication
+#include <LiquidCrystal_I2C.h>  // Library for LCD
 /*
 sensor Pin3 OxySen (TX) to Arduino Pin 10(RX)
-sensor Pin4 OxySen (RX) to Arduino Pin 11(TX)
+sensor Pin4 OxySen (RX) to Arduino Pin 9(TX)
 */
-
-SoftwareSerial mySerial(10, 11); // RX, TX
+#define led_pin 13
+SoftwareSerial mySerial(10, 11);  // RX, TX
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 
 const byte numChars = 43;
 char receivedChars[numChars];
-char tempChars[numChars];     // temporary array for use when parsing
-      
+char tempChars[numChars];  // temporary array for use when parsing
+
 float oxygen = 0.0;  // variables to hold the parsed data
 float tempo = 0.0;
 int pressure = 0;
@@ -35,144 +24,194 @@ boolean newData = false;
 
 unsigned long currentMillis = 0;
 unsigned long previousReadOxySenMillis = 0;
-const long ReadOxySenInterval = 1000;
-
-
+const long ReadOxySenInterval = 500;
+bool dotVisible = false;
 
 // Set up countdown timer
-const int COUNTDOWN_SECONDS = 60; // Countdown for 60 seconds
-
-
+// const int COUNTDOWN_SECONDS = 30; // Countdown for 20 seconds
+const int COUNTDOWN_SECONDS = 30;
+// unsigned long countdownStartTime = 0;
 
 //============
 
 void setup() {
-    Serial.begin(9600);
-    mySerial.begin(9600);
-    lcd.init();
-    lcd.backlight();
-    // display.setBrightness(5);
-    // display.setSegments(allON);
-    // delay(2000);
-	// display.clear();
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  // lcd.write("Starting");
+  delay(100);
+  pinMode(led_pin, OUTPUT);
+  blink();
 }
 
 //============
-void(* resetFunc) (void) = 0; //declare reset function
-
-// =================
-
+// void(* resetFunc) (void) = 0; //declare reset function
+void resetFunc() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Restarting...");
+  delay(1000);
+  asm volatile("  jmp 0");
+}
 void loop() {
-    currentMillis = millis();  // capture the latest value of millis()
+  currentMillis = millis();  // capture the latest value of millis()
+  if (currentMillis >= COUNTDOWN_SECONDS*1000*2) {
+      resetFunc();
+  }
 
-    if (currentMillis >= COUNTDOWN_SECONDS*1000) { // Update once per second
-        lcd.setCursor(0,0);
-        lcd.print("Restarting...  ");
-        lcd.setCursor(0,1);
-        lcd.print("(Every ");lcd.print(COUNTDOWN_SECONDS);lcd.print(" sec)");
-        delay(4000);
-        resetFunc(); // Reboot the Arduino
-    }
-
-    updateReadOxySen();
+  updateReadOxySen();
+  // delay(1000);
 }
 
 //============
+void blink() {
+
+  digitalWrite(led_pin, HIGH);
+  delay(1000);
+  digitalWrite(led_pin, LOW);
+}
+
+
 
 void recvWithStartEndMarkers() {
-    static boolean recvInProgress = false;
-    static byte ndx = 0;
-    char startMarker = 'O';
-    //char endMarker = 'e';
-    char endMarker = '\n';
-    char rc;
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = 'O';
+  char endMarker = '\n';
+  int rc;
 
-    while (mySerial.available() > 0 && newData == false) {
-        rc = mySerial.read();
+  while (mySerial.available() > 0 && newData == false) {
+    rc = mySerial.read();
 
-        if (recvInProgress == true) {
-            if (rc != endMarker) {
-                receivedChars[ndx] = rc;
-                ndx++;
-                if (ndx >= numChars) {
-                    ndx = numChars - 1;
-                }
-            }
-            else {
-                receivedChars[ndx] = '\0'; // terminate the string
-                recvInProgress = false;
-                ndx = 0;
-                newData = true;
-            }
+
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
         }
+      } else {
 
-        else if (rc == startMarker) {
-            recvInProgress = true;
-        }
+        receivedChars[ndx] = '\0';  // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
     }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
 }
 
 //============
 
-void parseData() {      // split the data into its parts
+void parseData() {  // split the data into its parts
 
-    char * strtokIndx; // this is used by strtok() as an index
+  char* strtokIndx;  // this is used by strtok() as an index
+  float tmp_tempo, tmp_percentage;
+  //data from sensor send looks like: "O xxxx.x T yxx.x P xxxx % xxx.xx e xxxx\r\n"
+  //e.g. "O 0020.1 T +19.3 P 1013 % 020.16 e 0001\r\n"
 
-    //data from sensor send looks like: "O xxxx.x T yxx.x P xxxx % xxx.xx e xxxx\r\n"
-    //e.g. "O 0020.1 T +19.3 P 1013 % 020.16 e 0001\r\n" 
+  // Use strtok() to extract the oxygen value
+  strtokIndx = strtok(tempChars, "T");
+  if (strtokIndx != NULL) {
+    oxygen = atof(strtokIndx);
+  }
 
-    strtokIndx = strtok(tempChars,"T");      // get the first part - the string
-    oxygen = atof(strtokIndx); // copy it to oxygen
-    
-    strtokIndx = strtok(NULL, "P"); // this continues where the previous call left off
-    tempo = atof(strtokIndx);     // convert this part to a float
+  // Use strtok() to extract the temperature value
+  strtokIndx = strtok(NULL, "P");
+  if (strtokIndx != NULL) {
+    tmp_tempo = atof(strtokIndx);
+  }
 
-    strtokIndx = strtok(NULL, "%"); // this continues where the previous call left off
-    pressure = atoi(strtokIndx);     // convert this part to an integer
+  // Use strtok() to extract the pressure value
+  strtokIndx = strtok(NULL, "%");
+  if (strtokIndx != NULL) {
+    pressure = atoi(strtokIndx);
+  }
 
-    strtokIndx = strtok(NULL, "e"); // this continues where the previous call left off
-    percentage = atof(strtokIndx);     // convert this part to a float
+  // Use strtok() to extract the percentage value
+  strtokIndx = strtok(NULL, "e");
+  if (strtokIndx != NULL) {
+    tmp_percentage = atof(strtokIndx);
+  }
 
-    strtokIndx = strtok(NULL, "\n"); // this continues where the previous call left off
-    errorox = atoi(strtokIndx);     // convert this part to an integer
-    
+  // Use strtok() to extract the errorox value
+  strtokIndx = strtok(NULL, "\n");
+  if (strtokIndx != NULL) {
+    errorox = atoi(strtokIndx);
+  }
+
+  // Check whether the formatting of the string matches the expected format
+  if (tmp_tempo >= 0 && tmp_percentage >= 0 && tmp_percentage <= 100) {
+    // If the formatting matches, assign the temporary variables to the real variables
+    tempo = tmp_tempo;
+    percentage = tmp_percentage;
+  }
+
+
+
 }
 
 //============
 
 void showParsedData() {
-    Serial.print("Oxygen ");
+  /*    Serial.print("Oxygen ");
     Serial.println(oxygen);
-    Serial.print("TemperatureO ");
+    Serial.print("Temperature ");
     Serial.println(tempo);
     Serial.print("Pressure ");
     Serial.println(pressure);
     Serial.print("Percent ");
-    Serial.println(percentage); 
+    Serial.println(percentage);
     Serial.print("Error from sensor ");
     Serial.println(errorox);   
-    Serial.println("");   
-    lcd.setCursor(0,0);
-    lcd.print("Oxygen ");lcd.print(percentage); lcd.print("%");
-    lcd.setCursor(0,1);
-    lcd.print("Temp. ");lcd.print(tempo); lcd.print((char)223); lcd.print("C ");
-    // display.showNumberDec(percentage);	
+*/
+  lcd.setCursor(0, 0);
+  lcd.print("Oxygen ");
+  lcd.print(percentage);
+  if (dotVisible) {
+    lcd.print("%  |       ");
+  } else {
+    lcd.print("%  -      ");
+  }
+  lcd.setCursor(0, 1);
+  lcd.print("Temp. ");
+  lcd.print(tempo);
+  lcd.print((char)223);
+  // lcd.print("C ");
+    if (dotVisible) {
+    lcd.print("C  -       ");
+  } else {
+    lcd.print("C  |      ");
+  }
+  if (percentage > 0.1) {         // if value is less than
+    digitalWrite(led_pin, HIGH);  // set LED pin to HIGH
+  } else {                        // if value is greater than or equal to
+    digitalWrite(led_pin, LOW);   // set LED pin to LOW
+  }
 }
 
-
 void updateReadOxySen() {
-    if (currentMillis - previousReadOxySenMillis >= ReadOxySenInterval) {
-          // time is up, so make anew read
-        recvWithStartEndMarkers();
-        if (newData == true) {
-            strcpy(tempChars, receivedChars);
-                // this temporary copy is necessary to protect the original data
-                //   because strtok() used in parseData() replaces the commas with \0
-            parseData();
-            showParsedData();
-            newData = false;
-        }           
-          // and save the time of change
-       previousReadOxySenMillis += ReadOxySenInterval;
+  if (currentMillis - previousReadOxySenMillis >= ReadOxySenInterval) {
+    // time is up, so make anew read
+    recvWithStartEndMarkers();
+    if (newData == true) {
+      strcpy(tempChars, receivedChars);
+      // this temporary copy is necessary to protect the original data
+      //   because strtok() used in parseData() replaces the commas with \0
+      parseData();
+      showParsedData();
+      newData = false;
+      dotVisible = !dotVisible;
     }
+    // and save the time of change
+    previousReadOxySenMillis += ReadOxySenInterval;
+    
+  }
 }
